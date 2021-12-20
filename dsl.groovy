b87e5bb2d0051a8a698f95eef7612255b06c5d82
comrider <cena3799@gmail.com>
2021-12-20 23:27:51 +0530

    new copy of java-tomcat-sample is attached

--- dsl.groovy
+++ dsl.groovy
@@ -1,13 +1,13 @@
 job ('new dsl job'){
     discription ('this is a complete ASL project ${ENV.BUILD_ID}')
     scm{
-        git ('https://github.com/comrider/ias-test.git', master)
+        git ('https://github.com/comrider/ias-test.git', 'master')
     }
     trigger{
-        scm ( * * * * *)
+        scm ( '* * * * *' )
     }
     steps{
-        mvn('clean package','pom.xml')
+        mvn('clean package','maven-samples/single-module/pom.xml')
     }
     publish{
         archiveArtifacts artifacts '**/*.war'
